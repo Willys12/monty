6c176a62c24ba95459dc842d24bb0de72b3a7b12
Willys12 <ben.willys92@gmail.com>
2023-12-15 16:44:39 +0300

    Update

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 int main(int argc, char *argv[])
 {
 int n;
+FILE *file = NULL;
+unsigned int line_number = 1;
 char *str = NULL;
 size_t len = 0;
 ssize_t n_read;
@@ -19,10 +21,10 @@ if (argc != 2)
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
-FILE *file = fopen(argv[1], "r");
+file = fopen(argv[1], "r");
 if (file == NULL)
 {
-fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+fprintf(stderr, "Error: Can't open file %s\n", argv[0]);
 exit(EXIT_FAILURE);
 }
 while ((n_read = getline(&str, &len, file)) != -1)
@@ -42,13 +44,14 @@ push(&stack, n);
 }
 else if (strcmp(opcode, "pall") == 0)
 {
-pall(&stack);
+pall(&stack, line_number);
 }
 else
 {
 fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 exit(EXIT_FAILURE);
 }
+line_number++;
 }
 free(str);
 fclose(file);
